c9b105fb311b689e7a4c44df6f3a5715eb1f99ce
AndreyITMetatech <andrey.bytko@gmail.com>
2023-05-08 15:46:52 +0300

    Внёс улучшения

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('burger').addEventListener('click', function(){
-        document.querySelector('header').classList.toggle('open')
-    })
-});
+let burgerMenu = document.querySelector('.header__burger-btn');
+let burgerList = document.querySelector('.header');
+
+burgerMenu.addEventListener('click', mobileBurger);
+
+function mobileBurger() {
+    burgerList.classList.toggle('open');
+}
